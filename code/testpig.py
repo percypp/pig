3e514dab1684558e737bd53601bc8258c53d8aaf
percypp <56879597+percypp@users.noreply.github.com>
2020-11-16 10:39:07 +0800

    Add files via upload
    testpig with more output

--- code/testpig.py
+++ code/testpig.py
@@ -12,19 +12,30 @@ import read_write as rw
 import img_tool as imt
 import greenplane_reddot as gprd
 import pointcloud as pc
-filename="20c2"
+import removebg_pig as rbgp
+filename="24c1"
 debug=False
 camera=filename[-2:]
 red=[]
 rec=[]
 tfr=[]
-rw.readallframes("C:/Users/gordon/Documents/ALLBAG/view_one/"+filename+".bag",0,100,red,rec,tfr )
+rw.readallframes("/Users/percychien/Desktop/project/pig-main/test/"+filename+".bag",0,100,red,rec,tfr )
 print("finish read")
-for i in range(100):
-    if(tfr[i]-tfr[0]>3100):
-        index=i
-        break
-allp=rw.savetopointcloud("",red[index],rec[index],re=True)
-vx,vy,vz,p=rw.load_vec(filename)
-allp=pc.transform(allp,vx,vy,vz,p,debug=debug)
-rw.savepointtofile(allp,filename+"pig.ply")
+
+def save(x):
+    num = x+1
+    x = (x*1000) + 1600
+    for i in range(100):
+        if(tfr[i]-tfr[0]>x):
+            index=i
+            break
+    vx,vy,vz,p=rw.load_vec(filename)
+    background = np.load(filename+"_bg.npy")
+    finalmask = rbgp.get_pig_mask(red[index], background)
+    allp=rw.savetopointcloud("",finalmask*red[index],rec[index],re=True)
+    allp=pc.transform(allp,vx,vy,vz,p,debug=debug)
+    cv2.imwrite(filename+"pig"+str(num)+".jpg", rec[index])
+    rw.savepointtofile(allp,filename+"pig"+str(num)+".ply")
+
+for i in range(5):
+    save(i)
