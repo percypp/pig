d3e0c61bed428f9cdf0cf2970fb4675cd7e712eb
percypp <56879597+percypp@users.noreply.github.com>
2020-11-16 10:40:25 +0800

    Add files via upload
    test with different path

--- code/test.py
+++ code/test.py
@@ -12,20 +12,20 @@ import read_write as rw
 import img_tool as imt
 import greenplane_reddot as gprd
 import pointcloud as pc
-filename="20c2"
+filename="24c1"
 camera=filename[-2:]
 debug=True
 red=[]
 rec=[]
 tfr=[]
-rw.readallframes("C:/Users/gordon/Documents/ALLBAG/view_one/"+filename+".bag",0,50,red,rec,tfr )
+rw.readallframes("/Users/percychien/Desktop/project/pig-main/test/"+filename+".bag",0,50,red,rec,tfr )
 print(tfr)
 bg=imt.getbackground(red[0:2])
 rbg=imt.remove_background(bg)
 red=[]
 rec=[]
 tfr=[]
-rw.readallframes("C:/Users/gordon/Documents/ALLBAG/view_one/board"+camera+".bag",0,50,red,rec,tfr )
+rw.readallframes("/Users/percychien/Desktop/project/pig-main/test/board/before"+filename+".bag",0,50,red,rec,tfr )
 gpd,gpc=rbg.remove_bg(red[20],rec[20])
 gp=imt.green_mask(gpc)
 rd=imt.red_mask(gpc)
@@ -36,7 +36,7 @@ if(debug==True):
     cv2.imshow('My Image3', rd*200)
 
 gp,reddot=gprd.find_green_plane_red_dot(gp,rd,gpc,debug)
-re=pc.findvec(gp,reddot,red[29],rec[29],debug=debug)
+re=pc.findvec(gp,reddot,red[20],rec[20],debug=debug)
 vx=re[0]
 vy=re[1]
 vz=re[2]
